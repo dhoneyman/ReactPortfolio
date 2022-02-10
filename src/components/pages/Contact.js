201df5c29863deef912069437302caf29cb04375
Dylan Honeyman <dylan@dylans-iMac.local>
2022-02-10 10:46:12 -0600

    contact form fixed

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import Modal from 'react-modal';
 // import { Form, Input, TextArea, Button } from 'semantic-ui-react';
@@ -12,14 +12,25 @@ const customStyles = {
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
-    backgroundColor: 'rgba(88, 123, 127, .75)',
+    backgroundColor: 'white',
+    background: ('#fff')
   },
 };
+
+const h2style = {
+  color: 'black'
+}
+
+
 
 
 
 export default function Contact() {
   Modal.setAppElement('#root');
+
+  const [submitter, setSubmitter] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   let subtitle;
   const [modalIsOpen, setIsOpen] = React.useState(false);
@@ -45,16 +56,25 @@ export default function Contact() {
   
   const form = useRef();
 
-  // const sendEmail = (e) => {
-  //   e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-  //   emailjs.sendForm('service_xx7292k', 'template_v5tzbva', form.current, 'user_soItZpUydlJ93Qgo5jfky')
-  //     .then((result) => {
-  //       console.log(result.text);
-  //     }, (error) => {
-  //       console.log(error.text);
-  //     });
-  // };
+    emailjs.sendForm('service_xx7292k', 'template_v5tzbva', form.current, 'user_soItZpUydlJ93Qgo5jfky')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
+  function formSubmition(e) {
+    openModal();
+    sendEmail(e);
+    setEmail('');
+    setSubmitter('');
+    setMessage('');
+
+  }
   return (
     <div className='uk-container'>
       <div className='uk-grid uk-child-width-expand@s uk-text-center'>
@@ -64,7 +84,7 @@ export default function Contact() {
             <h2>Dylan Honeyman</h2>
           </div>
           <div className='container uk-text-left'>
-            <p>Want to reach out? Fill out the form or send me an email at <a href="mailto:dylanhoneyman@gmail.com">dylanhoneyman@gmail.com</a>. I'm always on the hunt for new and exciting projects so dont hesitate to reach out. </p>
+            <p>Want to reach out? Fill out the form or send me an email at <a href="mailto:dylanhoneyman@gmail.com">dylanhoneyman@gmail.com</a>. I'm always on the hunt for new and exciting projects so don't hesitate to reach out. </p>
           </div>
         </div>
 
@@ -78,29 +98,47 @@ export default function Contact() {
       <input type="submit" value="Send" />
     </form> */}
 
-        <form ref={form} 
-        // onSubmit={sendEmail} 
+        <form  ref={form} 
+        onSubmit={formSubmition} 
         className='uk-padding uk-width-1-2'>
           <fieldset className="uk-fieldset">
 
-            <legend className="uk-legend">lets chat</legend>
+            <legend className="uk-legend">let's chat</legend>
 
             <div className="uk-margin">
-              <input name='from_name' className="uk-input" type="text" placeholder="Name" required />
+              <input onChange={(e) => setSubmitter(e.target.value)}
+               value={submitter}
+               name='from_name' 
+               className="uk-input" 
+               type="text" 
+               placeholder="Name" 
+               required />
             </div>
 
             <div className="uk-margin">
-              <input name='reply_to' className="uk-input" type="email" placeholder="Email" required />
+              <input onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              name='reply_to' 
+              className="uk-input" 
+              type="email" 
+              placeholder="Email" 
+              required />
             </div>
 
             <div className="uk-margin">
-              <textarea name='message' className="uk-textarea" rows="5" placeholder="Message"></textarea>
+              <textarea onChange={(e) => setMessage(e.target.value)}
+              value={message}
+              name='message' 
+              className="uk-textarea" 
+              rows="5" 
+              placeholder="Message">
+              </textarea>
             </div>
           </fieldset>
    
           <p className='uk-margin'>
             <button 
-            onClick={openModal} 
+            // onClick={openModal} 
             id='submit'type='submit' className="uk-button uk-button-default">Send</button>
 
       
@@ -111,7 +149,7 @@ export default function Contact() {
         style={customStyles}
         contentLabel="Example Modal"
         >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>You'll hear from me soon!</h2>
+        <h2 style={h2style} ref={(_subtitle) => (subtitle = _subtitle)}>You'll hear from me soon!</h2>
         
       </Modal>
         </p>

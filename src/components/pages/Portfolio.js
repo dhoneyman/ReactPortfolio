120968b614f6359ed9bc46e254691e2bbec79907
Dylan Honeyman <dylan@dylans-iMac.local>
2022-03-24 13:05:15 -0500

    portfolio update

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const styles = {
+    // images: {
+    //     maxWidth: '475px'
+    // }
+}
+
 
 export default function Portfolio() {
     return (
@@ -8,7 +14,7 @@ export default function Portfolio() {
         <div className="uk-child-width-expand@s uk-text-center uk-grid">
             <div className='uk-padding uk-width-1-2'>
                 <div className="uk-inline-clip uk-transition-toggle uk-light" tabindex="0">
-                    <a href='https://paddle-auction.herokuapp.com/' target="blank"><img className='thumbnail' src={process.env.PUBLIC_URL + "/pass.png"} alt="" /></a>
+                    <a href='https://paddle-auction.herokuapp.com/' target="blank"><img style={styles.images} className='thumbnail' src={process.env.PUBLIC_URL + "/pass.png"} alt="" /></a>
                     <a href='https://paddle-auction.herokuapp.com/' target="blank"><div className="uk-position-bottom ">
                         <a href='https://github.com/dhoneyman/project-3' target="blank"><span className="uk-transition-fade light-gh-icon uk-padding" uk-icon="icon: github; ratio: 2"></span></a>
                     </div></a>
@@ -18,7 +24,7 @@ export default function Portfolio() {
             </div>
             <div className='uk-padding uk-width-1-2'>
                 <div className="uk-inline-clip uk-transition-toggle uk-light" tabindex="0">
-                    <a href='https://climb-base.herokuapp.com/' target="blank"><img className='thumbnail' src={process.env.PUBLIC_URL + "/cbss3.png"} alt="" /></a>
+                    <a href='https://climb-base.herokuapp.com/' target="blank"><img style={styles.images} className='thumbnail' src={process.env.PUBLIC_URL + "/cbss3.png"} alt="" /></a>
                     <a href='https://climb-base.herokuapp.com/' target="blank"><div className="uk-position-bottom ">
                         <a href='https://github.com/dhoneyman/Climb-Base' target="blank"><span className="uk-transition-fade light-gh-icon uk-padding" uk-icon="icon: github; ratio: 2"></span></a>
                     </div></a>
@@ -28,7 +34,17 @@ export default function Portfolio() {
             </div>
             <div className='uk-padding uk-width-1-2'>
                 <div className="uk-inline-clip uk-transition-toggle uk-light" tabindex="0">
-                    <a href='https://dhoneyman.github.io/MyDashboard/' target="blank"><img className='thumbnail' src={process.env.PUBLIC_URL + "/myss1.png"} alt="" /></a>
+                    <a href='https://my-spotify-data-visualizer.herokuapp.com/' target="blank"><img style={styles.images} className='thumbnail' src={process.env.PUBLIC_URL + "/ssspot1.png"} alt="" /></a>
+                    <a href='https://my-spotify-data-visualizer.herokuapp.com/' target="blank"><div className="uk-position-bottom ">
+                        <a href='https://github.com/dhoneyman/SpotifyDataVisualization' target="blank"><span className="uk-transition-fade light-gh-icon uk-padding" uk-icon="icon: github; ratio: 2"></span></a>
+                    </div></a>
+                </div>
+                    <h3>My Spotify Data Visualizer</h3>
+                    <p>Utilizing Spotify's OAuth API, this web app distplays a user's listening history in a clean modifiable view. </p>
+            </div>
+            <div className='uk-padding uk-width-1-2'>
+                <div className="uk-inline-clip uk-transition-toggle uk-light" tabindex="0">
+                    <a href='https://dhoneyman.github.io/MyDashboard/' target="blank"><img style={styles.images} className='thumbnail' src={process.env.PUBLIC_URL + "/myss1.png"} alt="" /></a>
                     <a href='https://dhoneyman.github.io/MyDashboard/' target="blank"><div className="uk-position-bottom uk-padding">
                     <a href='https://github.com/dhoneyman/MyDashboard' target="blank"><span className="uk-transition-fade" uk-icon="icon: github; ratio: 2"></span></a>
                     </div></a>
@@ -38,7 +54,7 @@ export default function Portfolio() {
             </div>
             <div className='uk-padding uk-width-1-2'>
                 <div className="uk-inline-clip uk-transition-toggle uk-light" tabindex="0">
-                    <a href='https://dhoneyman.github.io/WeatherApp/' target="blank"><img className='thumbnail' src={process.env.PUBLIC_URL + "/wbss1.png"} alt="" /></a>
+                    <a href='https://dhoneyman.github.io/WeatherApp/' target="blank"><img style={styles.images} className='thumbnail' src={process.env.PUBLIC_URL + "/wbss1.png"} alt="" /></a>
                     <a href='https://dhoneyman.github.io/WeatherApp/' target="blank"><div className="uk-position-bottom uk-padding">
                         <a href='https://github.com/dhoneyman/WeatherApp' target="blank"><span className="uk-transition-fade light-gh-icon" uk-icon="icon: github; ratio: 2"></span></a>
                     </div></a>
@@ -48,7 +64,7 @@ export default function Portfolio() {
             </div>
             <div className='uk-padding uk-width-1-2'>
                 <div className="uk-inline-clip uk-transition-toggle uk-light" tabindex="0">
-                    <a href='https://arcane-caverns-20037.herokuapp.com/' target="blank"><img className='thumbnail' src={process.env.PUBLIC_URL + "/jatess.png"} alt="" /></a>
+                    <a href='https://arcane-caverns-20037.herokuapp.com/' target="blank"><img style={styles.images} className='thumbnail' src={process.env.PUBLIC_URL + "/jatess.png"} alt="" /></a>
                     <a href='https://arcane-caverns-20037.herokuapp.com/' target="blank"><div className="uk-position-bottom uk-padding">
                     <a href='https://github.com/dhoneyman/TextEditor' target="blank"><span className="uk-transition-fade" uk-icon="icon: github; ratio: 2"></span></a>
                     </div></a>
@@ -58,7 +74,7 @@ export default function Portfolio() {
             </div>
             <div className='uk-padding uk-width-1-2'>
                 <div className="uk-inline-clip uk-transition-toggle uk-light" tabindex="0">
-                    <a href='https://github.com/dhoneyman/TeamProfileGenerator' target="blank"><img className='thumbnail' src={process.env.PUBLIC_URL + "/cgss.png"} alt="" /></a>
+                    <a href='https://github.com/dhoneyman/TeamProfileGenerator' target="blank"><img style={styles.images} className='thumbnail' src={process.env.PUBLIC_URL + "/cgss.png"} alt="" /></a>
                     <a href='https://github.com/dhoneyman/TeamProfileGenerator' target="blank"><div className="uk-position-bottom uk-padding">
                     <a href='https://github.com/dhoneyman/TeamProfileGenerator' target="blank"><span className="uk-transition-fade" uk-icon="icon: github; ratio: 2"></span></a>
                     </div></a>
@@ -68,7 +84,7 @@ export default function Portfolio() {
             </div>
             <div className='uk-padding uk-width-1-2'>
                 <div className="uk-inline-clip uk-transition-toggle uk-light" tabindex="0">
-                    <a href='https://github.com/dhoneyman/EmployeeDatabaseManager/' target="blank"><img className='thumbnail' src={process.env.PUBLIC_URL + "/edbss1.png"} alt="" /></a>
+                    <a href='https://github.com/dhoneyman/EmployeeDatabaseManager/' target="blank"><img style={styles.images} className='thumbnail' src={process.env.PUBLIC_URL + "/edbss1.png"} alt="" /></a>
                     <a href='https://github.com/dhoneyman/EmployeeDatabaseManager/' target="blank"><div className="uk-position-bottom uk-padding">
                     <a href='https://github.com/dhoneyman/EmployeeDatabaseManager/' target="blank"><span className="uk-transition-fade" uk-icon="icon: github; ratio: 2"></span></a>
                     </div></a>
